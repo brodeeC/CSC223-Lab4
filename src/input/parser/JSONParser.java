94a46d401926082d7f5ff9ac0fe9726f40bb6b3f
CollinRiddle <mr5542015@gmail.com>
2024-02-13 15:33:23 -0500

    Ignoring Files

--- src/input/parser/JSONParser.java
+++ src/input/parser/JSONParser.java
@@ -35,6 +35,7 @@ public class JSONParser
 		JSONObject  JSONroot = (JSONObject)tokenizer.nextValue();
 
         // TODO: Build the whole AST, check for return class object, and return the root
+		// Change
 	}
 
     // TODO: implement supporting functionality
